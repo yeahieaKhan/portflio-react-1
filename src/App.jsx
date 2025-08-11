60d0494b530242a1fa639ebad2b13093772be9e4
yeahieaKhan <yeahieakhan2017@gmail.com>
2025-08-12 00:12:22 +0600

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Skills from "./components/Skills/Skills";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -12,6 +10,7 @@ function App() {
       <Navbar></Navbar>
       <Hero></Hero>
       <Skills></Skills>
+      <Footer></Footer>
     </>
   );
 }

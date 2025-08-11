8521c7ec141d928028921f85e4d3d52a9a9445dd
yeahieaKhan <yeahieakhan2017@gmail.com>
2025-08-11 21:13:42 +0600

    hero section done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,57 @@
 import React from "react";
 import HeroImg from "../../assets/hero-img.jpg";
+import { Typewriter } from "react-simple-typewriter";
 
 const Hero = () => {
+  const handleType = (count) => {
+    console.log(count); // logs the typing progress
+  };
+
+  const handleDone = () => {
+    console.log("Done after 5 loops!");
+  };
+
   return (
-    <div>
-      <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <img src={HeroImg} className="max-w-sm rounded-lg shadow-2xl" />
-          <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src={HeroImg} className="max-w-sm rounded-lg shadow-2xl" />
+        <div>
+          <h1 className="text-5xl font-bold">Hi, I'm YEAHIEA KHAN!</h1>
+          <p
+            style={{
+              paddingTop: "2rem",
+              fontWeight: "normal",
+              fontSize: "1.5rem",
+            }}
+          >
+            <span style={{ color: "red", fontWeight: "bold" }}>
+              <Typewriter
+                words={[
+                  "MERN Stack developer",
+                  "Frontend Developer",
+                  "Web Developer",
+                  "Full Stack Developer",
+                ]}
+                loop={5}
+                cursor
+                cursorStyle="_"
+                typeSpeed={70}
+                deleteSpeed={50}
+                delaySpeed={1000}
+                onLoopDone={handleDone}
+                onType={handleType}
+              />
+            </span>
+          </p>
+          <p className="py-6">
+            I specialize in creating full-stack web applications using{" "}
+            <b>
+              JavaScript, React.js, Node.js, Express.js, Tailwind CSS, MongoDB,
+              Firebase
+            </b>
+            . I'm always eager to learn and take on new challenges.
+          </p>
+          <button className="btn btn-primary">Contact Me</button>
         </div>
       </div>
     </div>
